b76b6ca2df07b9a22ed61d1bc2049fa6b17976d8
MH.Movasaghi <mmghho77@gmail.com>
2019-02-08 15:42:56 +0330

    Add Notation To First message from server

---
+++ noskhe_drugstore_app/noskhe_drugstore_app/Models/Notation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noskhe_drugstore_app.Models
+{
+    public class Notation
+    {
+        public int NotationId { get; set; }
+        public BrandType BrandPreference { get; set; }
+        public bool HasPregnancy { get; set; }
+        public bool HasOtherDiseases { get; set; }
+        public string Description { get; set; }        
+        public int ShoppingCartId { get; set; }
+    }
+}
