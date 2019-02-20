2d6aab39d682f89383f287820c9057ecca2d011d
MH.Movasaghi <mmghho77@gmail.com>
2019-02-20 18:48:11 +0330

    Toggle Button

---
+++ noskhe_drugstore_app/noskhe_drugstore_app/Models/CustomExceptions/TOGGLE_FAILURE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noskhe_drugstore_app.Models.CustomExceptions
+{
+    class TOGGLE_FAILURE : Exception
+    {
+        public TOGGLE_FAILURE()
+        {
+        }
+
+        public TOGGLE_FAILURE(string message) : base(message)
+        {
+        }
+
+        public TOGGLE_FAILURE(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
